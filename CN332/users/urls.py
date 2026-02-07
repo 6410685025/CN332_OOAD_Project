53d6f0c5375933c687fd28a483c290575a7b894d
ratcha <6410455015@cn311>
2026-02-07 14:32:24 +0700

    fix redirect logout

--- CN332/users/urls.py
+++ CN332/users/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('', views.dashboard_view, name='dashboard'),
     path('resident/', views.resident_dashboard, name='resident_dashboard'),
     path('staff/', views.staff_dashboard, name='staff_dashboard'),
