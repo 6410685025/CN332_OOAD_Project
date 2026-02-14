fc18f3023e8493cebf9a6a1f57447f2c767d64e8
ratcha <6410455015@cn311>
2026-02-14 22:24:57 +0700

    add technician feature

--- CN332/repairs/urls.py
+++ CN332/repairs/urls.py
@@ -4,8 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.repair_list_view, name='repair_list'),
     path('new/', views.create_repair_view, name='create_repair'),
+    path('history/', views.technician_work_history_view, name='technician_work_history'),
     path('<int:pk>/', views.repair_detail_view, name='repair_detail'),
     path('<int:pk>/assign/', views.assign_technician_view, name='assign_technician'),
     path('<int:pk>/update-status/', views.update_repair_status_view, name='update_repair_status'),
     path('<int:pk>/rate/', views.rate_repair_view, name='rate_repair'),
+    
+
 ]
